4c54cd114b78f0641e0cfaf62f6a2baea0077490
ZENO <zeno1030@naver.com>
2023-09-20 16:44:47 +0900

    회원 도메인 실행과 테스트

---
+++ src/test/java/hello/core/member/MemberAppTest.java
@@ -0,0 +1,12 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberAppTest {
+
+    @Test
+    void main() {
+    }
+}
