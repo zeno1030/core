db8e6c23b0f31733ed57f43e2697bf034579485c
ZENO <zeno1030@naver.com>
2023-09-21 14:33:09 +0900

    DIP OCP íP 위반 사항 수ì 

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.AppConfig.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -8,7 +9,8 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
