f122359591535e3f215a8ae753214c633bbe7648
zeno1030 <zeno1030@naver.com>
2023-12-12 10:44:52 +0900

    인터페이스 InitializingBean ,DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
@@ -19,5 +22,14 @@ public class NetworkClient {
     }
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception{
+        connect();
+        call("초기화 연결 메세지");
+    }
+    @Override
+    public void destroy() throws Exception{
+        disconnect();
     }
 }
